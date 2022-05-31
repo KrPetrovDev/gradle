ee970fc50a8a941fa01909b4219e46b05df74a37
Kristian Petrov <kristian.petrov@e2ycommerce.com>
2022-05-31 11:18:57 +0200

    Logging Level

--- buildSrc/src/main/groovy/e2y/NewTask.groovy
+++ buildSrc/src/main/groovy/e2y/NewTask.groovy
@@ -1,12 +1,17 @@
 package e2y
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.logging.LogLevel
 import org.gradle.api.tasks.TaskAction
 
 abstract class NewTask extends DefaultTask {
 
     @TaskAction
     def generate(){
+        println "Before Change loggingLevel"
+
+        logging.captureStandardOutput(LogLevel.LIFECYCLE)
+
         def variable = System.getenv('HOME')
         println "New One"
         print "docker run image -P"+variable+" -P otro 8080:8080"
